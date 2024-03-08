c97331f7d8a2d313f6b6f1a424b61cfffaeb5b66
David Villarreal <wvillar85769@universidadean.edu.co>
2024-03-08 11:39:02 -0500

    Ajuste

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -19,8 +19,9 @@ export default function Login() {
 
   //=> Validación del campo email
   const [email, setEmail] = React.useState("");
-  const validateEmail = (value) =>
-    value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+
+  const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i)
+  
   const isInvalid = React.useMemo(() => {
     if (email === "") return false;
     return validateEmail(email) ? false : true;
